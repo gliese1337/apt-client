90e86af60e09f49b27f14cf25d5bf71a2c6998f1
Logan Kearsley <logan.kearsley@vivintsolar.com>
2019-09-27 10:42:18 -0600

    More tests

--- test/reader-test.js
+++ test/reader-test.js
@@ -1,15 +1,73 @@
 'use strict';
 
-const { readAptSource, parseIndex } = require('../dist/aptreader');
 
-describe('read and parse debian repository', () => {  
-  it('should return source package info', async function() {
+const fs = require('fs');
+const path = require("path");
+const Packages = fs.readFileSync(path.resolve(__dirname, './data/Packages'));
+const PackagesGZ = fs.readFileSync(path.resolve(__dirname, './data/Packages.gz'));
+const Sources = fs.readFileSync(path.resolve(__dirname, './data/Sources'));
+const SourcesGZ = fs.readFileSync(path.resolve(__dirname, './data/Sources.gz'));
+
+
+const nock = require('nock');
+function packages() {
+  nock('http://deb.debian.org/', {"encodedQueryParams":true})
+    .get('/debian/dists/buster/main/binary-all/Packages')
+    .reply(200, Packages);
+}
+
+function packagesgz() {
+  nock('http://deb.debian.org/', {"encodedQueryParams":true})
+    .get('/debian/dists/buster/main/binary-all/Packages')
+    .reply(400);
+  
+  nock('http://deb.debian.org/', {"encodedQueryParams":true})
+    .get('/debian/dists/buster/main/binary-all/Packages.gz')
+    .reply(200, PackagesGZ);
+}
+
+function sources() {
+  nock('http://deb.debian.org/', {"encodedQueryParams":true})
+    .get('/debian/dists/buster/main/source/Sources')
+    .reply(200, Sources);
+}
+
+function sourcesgz() {
+  nock('http://deb.debian.org/', {"encodedQueryParams":true})
+    .get('/debian/dists/buster/main/source/Sources')
+    .reply(400);
+  
+  nock('http://deb.debian.org/', {"encodedQueryParams":true})
+    .get('/debian/dists/buster/main/source/Sources.gz')
+    .reply(200, SourcesGZ);
+}
+
+const { readAptSource } = require('../dist/aptreader');
+
+describe('read and parse debian repository', () => {
+  afterEach(() => nock.restore());
+  
+  it('should return binary package info from plaintext index', async function() {
     this.timeout(20000);
-    await readAptSource('deb-src http://deb.debian.org/debian buster main contrib non-free');
+    packages();
+    await readAptSource('deb http://deb.debian.org/debian buster main');
   });
   
-  it('should return binary package info', async function() {
+  it('should return binary package info from gzipped index', async function() {
     this.timeout(20000);
-    await readAptSource('deb http://deb.debian.org/debian buster main contrib non-free');
+    packagesgz();
+    await readAptSource('deb http://deb.debian.org/debian buster main');
+  });
+  
+  it('should return source package info from plaintext index', async function() {
+    this.timeout(20000);
+    sources();
+    await readAptSource('deb-src http://deb.debian.org/debian buster main');
+  });
+    
+  it('should return source package info from gzipped index', async function() {
+    this.timeout(20000);
+    sourcesgz();
+    await readAptSource('deb-src http://deb.debian.org/debian buster main');
   });
 });
